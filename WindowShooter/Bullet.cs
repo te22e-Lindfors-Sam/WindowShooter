06be4addf37a248d7b009dc2d9786be3e43513f9
Sam Lindfors <sam.lindfors@elev.ga.ntig.se>
2023-11-08 08:25:27 +0100

    PlayerMovement

---
+++ WindowShooter/Bullet.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+using Raylib_cs;
+
+class Bullelt
+{
+    Vector2 pos;
+    int speed;
+
+    public Bullelt(Vector2 pos)
+    {
+        this.pos = pos;
+    }
+}
--- WindowShooter/Program.cs
+++ WindowShooter/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+using Raylib_cs;
+
+int windowX = 800, windowY = 800;
+Raylib.InitWindow(windowX, windowY, "WindowShooter");
+Raylib.SetTargetFPS(60);
+Player player = new Player(new Vector2(windowX/2,windowY/2), 20, 5);
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  
+  Raylib.ClearBackground(Color.BLACK);
+  
+
+    player.ControllPlayer(windowX, windowY);
+    player.renderPlayer();
+
+
+  Raylib.EndDrawing();
+}
